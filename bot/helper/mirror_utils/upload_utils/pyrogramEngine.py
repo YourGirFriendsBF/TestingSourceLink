754e29a37cdb5632ab6ef49317b3227e2f584316
YourGirFriendsBF <109566119+YourGirFriendsBF@users.noreply.github.com>
2022-08-17 23:09:14 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -39,6 +39,7 @@ class TgUploader:
         self.__sent_msg = app.get_messages(self.__listener.message.chat.id, self.__listener.uid)
         self.__user_settings()
         self.__leech_log = LEECH_LOG.copy()  # copy then pop to keep the original var as it is
+        self.__leech_log_alt = LEECH_LOG_ALT.copy()
         self.__app = app
         self.__user_id = listener.message.from_user.id
         self.isPrivate = listener.message.chat.type in ['private', 'group']
